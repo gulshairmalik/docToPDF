7407409f7a7c0130cb767e8665e4c0e88bd34705
Gulshair Malik <gulshairmalik@gmail.com>
2019-11-25 19:36:01 +0500

    v1.0

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const multer  = require('multer');
+const converter = require('office-converter')();
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null,file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
+
+//Setting View Engine
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+app.get('/convert',(req,res) => {
+    res.render('index');
+});
+
+app.post('/convert',upload.single('file'),(req,res) => {
+    
+    converter.generatePdf('public/uploads/'+req.file.originalname, function(err, result) {
+        if (result.status === 0) {
+          let outputFile = result.outputFile.replace('public/','');
+          let filename = outputFile.replace('uploads/','');
+          res.render('index',{file:outputFile});
+        }
+    });
+
+});
+
+app.listen(process.env.PORT || 3000);
